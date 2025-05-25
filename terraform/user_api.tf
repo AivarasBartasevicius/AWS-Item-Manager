614ccfed28d663adeb25115fa804cb7809a29e8d
AivarasBartasevicius <aivaras.bartasevicius@gmail.com>
2025-05-25 11:24:58 +0300

    arm maybe later

--- terraform/user_api.tf
+++ terraform/user_api.tf
@@ -5,8 +5,6 @@ resource "aws_lambda_function" "user_api" {
   function_name = "user_api"
 
   role = aws_iam_role.item_manager.arn
-
-  architectures = ["arm64"]
 }
 
 resource "aws_cloudwatch_log_group" "user_api" {
