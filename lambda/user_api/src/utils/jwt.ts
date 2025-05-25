97984aa356c89bfc216b464072523ced34f792d2
AivarasBartasevicius <aivaras.bartasevicius@gmail.com>
2025-05-25 13:13:56 +0300

    cleanup

--- lambda/user_api/src/utils/jwt.ts
+++ lambda/user_api/src/utils/jwt.ts
@@ -1,7 +1,6 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 
 export function verifyAccessToken(token: string) {
-  token = token.replace(/^bearer\s/gi, "");
   const verify = jwt.verify(
     token,
     process.env.ACCESS_TOKEN_SECRET as string
